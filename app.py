e3d6a6b951ff2608c7fc8cb7a63a61f6ab5d704b
The Floating String <laurencel2001@gmail.com>
2020-05-20 11:47:29 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return render_template("home.html")
+
+@app.route("/ask", methods=["GET", "POST"])
+def ask():
+	value = request.args.get("resp")
+	return "hello!" + ' ' +value
+
+if __name__ == '__main__':
+	app.run(debug=True)
